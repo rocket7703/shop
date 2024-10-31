4baa0c537c95f43ec1aad2c256f2bd10cc21c4ef
rocket7703 <aovtov@inbox.ru>
2024-10-31 17:44:46 +0300

    31.10.24

---
+++ src/components/CartItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="gap-4 flex border border-slate-200 p-4 rounded-xl">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
+
+    <div class="flex flex-col">
+      <p>Мужские кроссовки Nike</p>
+      <div class="flex justify-between">
+        <b>4 700 руб.</b>
+      </div>
+    </div>
+    <img class="opacity-30 hover:opacity-100 cursor-pointer" src="/close.svg" alt="" />
+  </div>
+</template>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DrawerHead from './DrawerHead.vue'
+import CartItem from './CartItem.vue'
+</script>
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-50"></div>
+  <div class="bg-white w-96 h-full fixed z-20 right-0 top-0">
+    <DrawerHead />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </div>
+</template>
