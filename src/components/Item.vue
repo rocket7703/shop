e5a09a6cd0ab541d7710dfd16bcf64e75e8fdbe0
rocket7703 <aovtov@inbox.ru>
2024-10-31 19:03:24 +0300

    31oct

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,23 +1,34 @@
 <script setup>
-defineProps ({
-    imageUrl:String,
+import { ref } from 'vue'
+
+defineProps({
+  title: String,
+  img: String,
+  price: Number
 })
+
+const isLiked = ref(false)
+
+const onClick = () => {
+  isLiked.value = !isLiked.value
+}
 </script>
 
 <template>
-    <div>
-    <div class="relative   bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 scale-105 hover:shadow-xl">
- <img src="/like-2.svg" alt="like 1" class="absolute top-8 left-8">
- <img :src="imageUrl" alt="Sneaker">
- <p> Мужские кроссовки Nike Blazer Mid Suede</p>
- <div class="flex justify-between">
-   <div class="flex flex-col ">
-    <span class="text-slate-500">Цена:</span>
-    <span>4700 руб</span>
-    
-   </div>
-   <img  src="/plus.svg" alt="Plus">
- </div>
+  <div
+    class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+  >
+    <div @click="onClick" class="absolute top-8 left-8">
+      <img :src="isLiked ? '/like-1.svg' : '/like-2.svg'" alt="Favorite" />
+    </div>
+    <img :src="img" class="w-full" alt="Sneaker" />
+    <p>{{ title }}</p>
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col gap-2">
+        <span class="text-slate-200">Цена:</span>
+        <span class="font-bold">{{ price }} руб.</span>
+      </div>
+      <img src="/plus.svg" alt="Plus" />
     </div>
   </div>
 </template>
--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,12 +1,20 @@
 <script setup>
 import Item from './Item.vue'
+
+defineProps({
+  items: Array
+})
 </script>
+
 <template>
   <div class="grid grid-cols-4 gap-5 mt-5">
-    <Item imageUrl="/sneakers/sneakers-1.jpg" />
-    <Item />
-    <Item />
-    <Item />
-    <Item />
+    <Item
+      title="Мужские Кроссовки Nike Blazer"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      isFavourite="false"
+      :onClickAdd="onClickAdd"
+      :isAdded="true"
+    />
   </div>
 </template>
